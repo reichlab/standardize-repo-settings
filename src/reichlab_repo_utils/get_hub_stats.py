1d38c5c6d25278bc9905acc238b9e953a0d66c62
Becky Sweger <rsweger@umass.edu>
2024-12-05 13:40:04 -0500

    Parallelize file count operations for better performance
    This changeset also adds a summary .csv file that groups
    file counts by hub and directory (e.g., model-output/target-data)

--- src/reichlab_repo_utils/get_hub_stats.py
+++ src/reichlab_repo_utils/get_hub_stats.py
@@ -50,7 +50,9 @@ To use this script:
 # ]
 # ///
 
+import concurrent.futures
 import os
+from collections import ChainMap
 from pathlib import Path
 from urllib.parse import urlsplit
 
@@ -80,6 +82,8 @@ except KeyError:
     raise ValueError("GITHUB_TOKEN environment variable is required")
 session = requests.Session()
 session.headers.update({"Authorization": f"token {token}", "Accept": "application/vnd.github.v3+json"})
+pl.Config(tbl_rows=500)
+pl.Config.set_fmt_str_lengths(100)
 # For testing, limit number of files to process. Set to 0 for no limit
 FILE_COUNT = 0
 
@@ -87,83 +91,86 @@ FILE_COUNT = 0
 ###########################################################
 # Actual work starts here                                 #
 ###########################################################
-def main(owner: str, repo: str, data_dir: str) -> pl.DataFrame:
+def main(owner: str, repo: str, data_dir: str) -> Path:
     output_dir = Path(data_dir)
     output_dir.mkdir(exist_ok=True)
 
+    repo_line_counts = pl.DataFrame()
+    for directory in ["model-output", "target-data"]:
+        files = list_files_in_directory(owner, repo, directory)
+        print(f"found {len(files)} files in {repo}/{directory}")
+        if len(files) == 0:
+            continue
+
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            futures = [executor.submit(count_rows, file) for file in files]
+
+        all_results = [f.result() for f in futures]
+        all_counts = dict(ChainMap(*all_results))
+        count_df = pl.DataFrame({"file": list(all_counts.keys()), "row_count": list(all_counts.values())})
+
+        if len(count_df) > 0:
+            count_df = count_df.with_columns(
+                pl.lit(directory).alias("dir"),
+                pl.lit(f"{owner}/{repo}").alias("repo"),
+            )
+            # extract team name from file name (it's actually the model_id, but renaming
+            # it now would require reprocessing a bunch of data)
+            count_df = count_df.with_columns(
+                pl.when(pl.col("dir") == "model-output").then(
+                    pl.col("file")
+                    .str.slice(11)
+                    .str.splitn(".", 2)
+                    .struct.rename_fields(["team", "file_type"])
+                    .struct.field("team")
+                )
+            )
+            repo_line_counts = pl.concat([repo_line_counts, count_df])
+
+    parquet_file = output_dir / f"{repo}.parquet"
+    repo_line_counts.write_parquet(parquet_file)
+    print(f"Saved {parquet_file}")
+    return parquet_file
+
+
+def count_rows(file_url) -> dict[str, int | None]:
+    """Returns a dataframe with a line count for each file a list."""
+    line_counts = {}
+
+    file_path = Path(urlsplit(file_url).path)
+    file_name = file_path.name
+    file_type = file_path.suffix
+
+    if file_type not in [".parquet", ".csv"]:
+        return dict()
+
+    try:
+        if file_type == ".csv":
+            count = count_rows_csv(file_url)
+        else:
+            count = count_rows_parquet(file_url)
+    except Exception as e:
+        print(f"Error processing {file_url}")
+        print(e)
+        count = None
+    finally:
+        line_counts[file_name] = count
+
+    return line_counts
+
+
+def count_rows_parquet(file_url: str) -> int:
+    """Use duckdb to get row count from parquet file metadata."""
     # duckdb is a handy way to access parquet file metadata
     # (we're not using it to store any output data)
     with duckdb.connect() as con:
         con.sql("INSTALL httpfs;")
-        con.sql("SET http_keep_alive=false;")
-
-        repo_line_counts = pl.DataFrame()
-        for directory in ["model-output", "target-data"]:
-            files = list_files_in_directory(owner, repo, directory)
-            if len(files) == 0:
-                continue
-
-            count_df = count_rows(con, files)
-            if len(count_df) > 0:
-                count_df = count_df.with_columns(
-                    pl.lit(directory).alias("dir"),
-                    pl.lit(f"{owner}/{repo}").alias("repo"),
-                )
-                # extract team name from file name (it's actually the model_id, but renaming
-                # it now would require reprocessing a bunch of data)
-                count_df = count_df.with_columns(
-                    pl.when(pl.col("dir") == "model-output").then(
-                        pl.col("file")
-                        .str.slice(11)
-                        .str.splitn(".", 2)
-                        .struct.rename_fields(["team", "file_type"])
-                        .struct.field("team")
-                    )
-                )
-                repo_line_counts = pl.concat([repo_line_counts, count_df])
-
-    repo_line_counts.write_parquet(output_dir / f"{repo}.parquet")
-    return repo_line_counts
-
-
-def count_rows(con, files) -> pl.DataFrame:
-    """Returns a dataframe with a line count for each file a list."""
-    file_counter = 0
-    line_counts = {}
-
-    for file_url in files:
-        if FILE_COUNT > 0 and file_counter > FILE_COUNT:
-            # if we've set FILE_COUNT for testing and have
-            # exceeded it, stop processing files
-            break
-        file_path = Path(urlsplit(file_url).path)
-        file_name = file_path.name
-        file_type = file_path.suffix
-
-        if file_type not in [".parquet", ".csv"]:
-            continue
-
-        try:
-            if file_type == ".csv":
-                count = count_rows_csv(file_url)
-            else:
-                count = count_rows_parquet(con, file_url)
-        except Exception:
-            print(f"Error processing {file_url}")
-            count = None
-        finally:
-            file_counter += 1
-            line_counts[file_name] = count
-
-    count_df = pl.DataFrame({"file": list(line_counts.keys()), "row_count": list(line_counts.values())})
-
-    return count_df
-
-
-def count_rows_parquet(con, file_url: str) -> int:
-    """Use duckdb to get row count from parquet file metadata."""
-    sql = f"SELECT num_rows FROM parquet_file_metadata('{file_url}');"
-    count = con.sql(sql).fetchone()[0]
+        sql = f"SELECT num_rows FROM parquet_file_metadata('{file_url}');"
+        if (query_result := con.sql(sql).fetchone()) is not None:
+            count = query_result[0]
+        else:
+            print(f"Unable to access parquet metadata for {file_url}")
+            count = 0
 
     return count
 
@@ -197,6 +204,9 @@ def list_files_in_directory(owner, repo, directory) -> list[str]:
                 files.append(item["download_url"])
             elif item["type"] == "dir":
                 files.extend(list_files_in_directory(owner, repo, item["path"]))
+            if FILE_COUNT > 0 and len(files) >= FILE_COUNT:
+                # file count is constrained for testing
+                break
 
         # Check if there is a next page
         if "next" in response.links:
@@ -204,30 +214,44 @@ def list_files_in_directory(owner, repo, directory) -> list[str]:
         else:
             url = None
 
-    print(f"found {len(files)} files in {repo}/{directory}")
     return files
 
 
-def write_csv(output_dir: Path) -> Path | None:
+def write_csv(output_dir: Path):
     """Write output of all hub stats .parquet files to .csv."""
     parquet_files = f"{str(output_dir)}/*.parquet"
     try:
+        # save detailed hub stats as .csv
         hub_stats = pl.read_parquet(parquet_files)
         csv_file = output_dir / "hub_stats.csv"
         hub_stats.write_csv(csv_file)
+        print(f"Saved hub details: {csv_file}")
+
+        # save a summarized version of hub stats
+        hub_stats_summary = hub_stats.sql("""
+            SELECT repo, dir, SUM(row_count) as row_count
+            FROM self
+            GROUP BY repo, dir
+            ORDER BY repo, dir
+        """)
+        summary_csv_file = output_dir / "hub_stats_summary.csv"
+        hub_stats_summary.write_csv(summary_csv_file)
+        print(f"Saved hub summaries: {summary_csv_file}")
+
+        # display summarized data on console for a quick gut check
+        print(hub_stats_summary)
+
     except pl.exceptions.ComputeError:
         print(f"Cannot create .csv: no parquet files found in {output_dir}")
-        csv_file = None
-
-    return csv_file
 
 
 if __name__ == "__main__":
     data_dir = Path(__file__).parent / "data" / "hub_stats"
+    if FILE_COUNT > 0:
+        print(f"Limiting file count to {FILE_COUNT} for testing")
     for repo_name in HUB_REPO_LIST:
         print(f"Getting stats for {repo_name}...")
         owner, repo = repo_name.strip().split("/")
         main(owner, repo, str(data_dir))
     print("Updating .csv to include data for all hubs...")
-    csv_file = write_csv(data_dir)
-    print(f"Hub stats saved to {csv_file}")
+    write_csv(data_dir)
